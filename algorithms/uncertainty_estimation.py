d7abaa293fad34b3372f5e2b1d23646420c991ce
pakhuiying <pakhuiying95@gmail.com>
2023-05-16 21:40:08 +0800

    added uncertainty for estimating glint extent and magnitude

--- algorithms/uncertainty_estimation.py
+++ algorithms/uncertainty_estimation.py
@@ -15,6 +15,7 @@ import matplotlib.patches as patches
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 from scipy import ndimage
+from scipy import stats
 import algorithms.SUGAR as sugar
 
 class UncertaintyEst:
@@ -50,3 +51,51 @@ class UncertaintyEst:
         corrected_im_background, gm_list_background, bg_list_background = correction_iterative(glint_image,iter=iter,bounds = bounds,estimate_background=True)
         corrected_im, gm_list, bg_list = correction_iterative(glint_image,iter=iter,bounds = bounds,estimate_background=False)
         
+
+def plot_glint_contour(im_aligned,glint_mask,NIR_band=3,add_weights=False):
+    """ 
+    :param im_aligned (np.ndarray) band-aligned image from:
+        RI = espect.ReflectanceImage(cap)
+        im_aligned = RI.get_aligned_reflectance()
+    :param glint_mask (np.ndarray): where 1 is glint, 0 is non-glint
+    :param NIR_band (int): band number e.g. NIR band to extract the glint mask
+    """
+    # use the NIR band
+    gm = glint_mask[:,:,NIR_band]
+    
+    nrow, ncol = gm.shape
+    y = np.linspace(0,nrow-1,nrow,dtype=int)[::5]
+    x = np.linspace(0,ncol-1,ncol,dtype=int)[::5]
+    X, Y = np.meshgrid(x,y)
+    # Y = np.flipud(Y)
+    xy = np.vstack([X.ravel(), Y.ravel()])
+    print(xy.shape)
+    idx = np.argwhere(gm==1)
+    # Xtrain = np.vstack([idx[:,0], idx[:,1]])
+    Xtrain = np.vstack([idx[:,1], idx[:,0]])
+
+    if add_weights is True:
+        im = im_aligned[:,:,NIR_band]
+        weights = im[(idx[:,0], idx[:,1])]#.reshape(-1,1)
+        # weights = np.flipud(weights)
+        weights[weights<0] = 0
+
+        # uses Scott's Rule to implement bandwidth selection
+        kernel = stats.gaussian_kde(Xtrain,weights=weights)
+    else:
+        kernel = stats.gaussian_kde(Xtrain)
+
+    Z = kernel(xy).T
+    print(Z.shape)
+    Z = np.reshape(Z, X.shape)
+    Z = np.flipud(Z)
+    # plot contours of the density
+    levels = np.linspace(0, Z.max(), 25)
+
+    fig, axes = plt.subplots(1,2,figsize=(10,5),sharex=True)
+    axes[0].imshow(np.take(im_aligned,[2,1,0],axis=2))
+    im = axes[1].contourf(X, Y, Z, levels=levels, cmap=plt.cm.RdGy_r)
+    axes[1].set_aspect('equal')
+    fig.colorbar(im,ax=axes[1])
+    plt.show()
+    return 
